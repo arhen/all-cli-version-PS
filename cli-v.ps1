209c88d8fb87e36d8d619f27bab555f790844a64
Rahmat Slamet <itsme.arhen@gmail.com>
2019-10-24 19:20:50 +0800

    add programming language checking

--- cli-v.ps1
+++ cli-v.ps1
@@ -11,10 +11,20 @@ Function Test-Command {
 
      Finally {$ErrorActionPreference=$oldPreference}
 }
+Write-Host '=== Programming Language CLI ==='
+Write-Host 'PHP      : '  -ForegroundColor Green; if(Test-Command php -v){php -v}
+write-Host ''
+Write-Host 'Python   : '  -ForegroundColor Red; if(Test-Command python --version){python --version}
+write-Host ''
+Write-Host 'Ruby     : '  -ForegroundColor blue; if(Test-Command ruby -v){ruby -v}
 
+Write-Host '---------------------------------------------------'
+
+Write-Host '=== Package Manager ==='
 Write-Host 'Dotnet      : ' -NoNewline; if(Test-Command dotnet --version){dotnet --version}
 Write-Host 'composer    : ' -NoNewline; if(Test-Command composer -V){composer -V}
 Write-Host 'npm         : ' -NoNewline; if(Test-Command npm -v){npm -v}
 Write-Host 'gulp        : ' ; if(Test-Command gulp -v){gulp -v}
 Write-Host 'yarn        : ' -NoNewline; if(Test-Command yarn -v){yarn -v}
 Write-Host 'pip         : ' -NoNewline; if(Test-Command pip --version){pip --version}
+Write-Host 'gem         : ' -NoNewline; if(Test-Command gem --version){gem --version}
